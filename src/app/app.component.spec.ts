ab91533f27c4952852d81d94ed09bcc55fec53fb
Samra Azeem <62462084+samraazeem@users.noreply.github.com>
2021-08-16 02:50:06 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule]
+    });
+    TestBed.compileComponents();
+  });
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
